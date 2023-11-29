660e0fa8f99b5ab077e7f1bfa714a68fb9538485
Lam_Edward <lame972@hsd.k12.or.us>
2023-11-29 10:10:04 -0800

    Fetching function and parse the api to the local database with the key 'data'

--- main.py
+++ main.py
@@ -4,14 +4,23 @@ A simple PyperCard app to get a user's name, and then display a friendly
 """
 # We need to use the a pypercard App.
 from pypercard import App
+# import requests
+import requests
 
+# import json
+import json
+
+#import pyodide_http
+import pyodide_http
 
 # Create the app as the object called hello_app.
-hello_app = App()
+weather_app = App()
 
+# Patch the Requests Libarary so it works with Pyscript
+pyodide_http.patch_all()
 
-# In the "get_name" card, when you "click" on the "submit" button...
-@hello_app.transition("get_name", "click", "submit")
+# In the "input" card, when you "click" on the "submit" button...
+@weather_app.transition("input-card", "click", "submit-btn")
 def hello(app, card):
     """
     Store the value in the card's input box, with the id "name", into the app's
@@ -19,18 +28,33 @@ def hello(app, card):
 
     Then transition to the "say_hello" card.
     """
-    app.datastore["name"] = card.get_by_id("name").value
-    return "say_hello"
+    app.datastore["city-input"] = card.get_by_id("city-input").value
+
+    # Required parameters to fetch the weather api...
+    parameters = {
+    'key': '64c4ff4519da47c6bf6224642231911',
+    'q': f'{app.datastore["city-input"]}',
+    'aqi': 'no'
+    }
+    
+    # Fetch the api...
+    api_call = requests.get("http://api.weatherapi.com/v1/current.json", params=parameters)
+
+    # parse the entire api as a json to the local database...
+    app.datastore["data"] = api_call.json()
 
 
-# In the "say_hello" card, when you "click" on the "again" button...
-@hello_app.transition("say_hello", "click", "again")
+    return "result-card"
+
+
+# In the "result_card" card, when you "click" on the "again" button...
+@weather_app.transition("result-card", "click", "again")
 def again(app, card):
     """
     Don't do anything except transition to the "get_name" card.
     """
-    return "get_name"
+    return "input-card"
 
 
 # Start the hello_app
-hello_app.start()
+weather_app.start()
